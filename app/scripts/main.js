f9495d3b03b7ca54ce3c3f02522d223fa83d3627
Elizabeth Schafer <elizabeth.schafer.wenk@gmail.com>
2013-04-06 09:45:02 -0400

    Using non-amd versions of backbone/underscore, using requirejs shim instead

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,24 @@
 require.config({
 	paths: {
 		"jquery": "vendor/jquery/jquery",
-		"underscore": "vendor/underscore-amd/underscore",
-		"backbone": "vendor/backbone-amd/backbone",
+		"underscore": "vendor/underscore/underscore",
+		"backbone": "vendor/backbone/backbone"
+	},
+	shim: {
+		"backbone": {
+
+			// These script dependencies should be loaded before loading backbone.js.
+			deps: [
+				"underscore",
+				"jquery"
+			],
+
+			// Once loaded, use the global `Backbone` as the module value.
+			exports: "Backbone"
+		},
+		"underscore": {
+			exports: "_"
+		}
 	}
 });
 
